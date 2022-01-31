480911e63ea49aac867f15a92e1c4d1b4ec2a684
“Philippe <philippe.henry@edu.esiee.fr>
2022-01-31 11:02:49 +0100

    [add] last commit

---
+++ P_D_E/application/routes.py
@@ -0,0 +1,122 @@
+from turtle import pd
+from application import app
+from flask import render_template, url_for, request
+import pandas as pd
+import json
+import plotly
+import plotly_express as px
+
+import sys
+
+sys.path.append("../")
+from P_D_E.application.getcounters import counter
+from P_D_E.application.best_players import getbestplayers
+from P_D_E.application.bluered import *
+
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb+srv://guigui:1234@cluster0.eeflg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+db = cluster["leagueoflegends"]
+collection_best_player = db["personnageslol"]
+Counters_lol = db["counters"]
+blue_red_lol = db["blue_red"]
+
+
+@app.route('/GetCounters', methods=['POST'])
+def callback_counter(champion):
+    counter_name = []
+    counter_golds =[]
+    iscountered_name = []
+    iscountered_golds = []
+
+    iscountered_name, iscountered_golds, counter_name, counter_golds =  counter(champion)
+
+@app.route("/")
+def index():
+
+    return render_template("index.html", title = "Home")
+
+@app.route("/Counters", methods=['get','post'])
+def Counters():
+    
+    # db base counters ~>
+    db = Counters_lol.find_one({"_id": 1})
+    db_1 = Counters_lol.find_one({"_id": 0})
+
+    loser = db["loser"]
+    winner = db_1["winner"]
+    # db base counters <~
+    
+    champion_search = request.form.to_dict()
+
+    counter_name = []
+    counter_golds =[]
+    iscountered_name = []
+    iscountered_golds =[]
+
+    if "counters" in champion_search:
+        iscountered_name, iscountered_golds, counter_name, counter_golds =   counter(champion_search["counters"])
+    else:
+        iscountered_name, iscountered_golds, counter_name, counter_golds =   counter("")
+
+
+    
+    if len(counter_name) == 0:
+        champs_counter_str = " "
+    else:
+        champs_counter_str = champion_search["counters"] + " is countered by : "
+
+    for champs_1 in counter_name:
+        champs_counter_str += champs_1 + " ."
+
+
+    
+    if len(iscountered_name) == 0:
+        champs_countered_str = " "
+    else:
+        champs_countered_str = champion_search["counters"] + "  counter : "
+
+
+    for champs in iscountered_name:
+        champs_countered_str += champs + " ." 
+
+    try:
+        if len(loser and winner) < 0:
+            print("return")
+    except Exception:
+        pass
+
+
+    return render_template("counters.html", champion=champs_countered_str, champion_1 = champs_counter_str)
+
+
+@app.route("/Dashboard")
+def Dashboard():
+    bestplayers = getbestplayers()
+    return render_template("index.html", title = "Home")
+
+@app.route("/Best Players")
+def Best_players():
+    
+    test = getbestplayers()
+    #print(test)
+
+    return render_template("best_players.html", best_players=test)
+
+
+
+
+
+@app.route("/blue-red")
+def stats_blue_red():
+    stats = stats_blue()
+
+
+    #print(stats)    
+
+    stats_blue
+    return render_template("blue-red.html", stats_blue=stats)
+
+
---
+++ README.md
@@ -0,0 +1,55 @@
+# Data Eng Project
+
+## Introduction
+	This project is about league of legends. He will show some statistics from league of legends (lol).
+
+1. PROJECT DONE WITH 
+	1. Visual Studio code : https://code.visualstudio.com/
+
+## User's guide
+
+### How to run the project
+
+1. First step, you need to clone it : "git clone adresse_publique_de_votre_projet"
+2. Second step, you have to run the requirements.txt you can run it with : "python -m pip install -r requirements.txt"
+3. Third step, you have a directory nammed P_D_E, go in.
+4. Fourth step, execute it with : "python main.py".
+
+### Packages 
+
+1. All the packages you need will be on the requirements.txt.
+2. Ip of our server by the website localhost:5000.
+
+## Developer's guide	
+
+### Main
+
+1. Our main will call the others functions.
+	1. You have a directory named P_D_E.
+	2. When you're in you have the main.py you need to run it to start the project.
+	3. Also you have a directory nammed application with the code.
+	4. When you're in you have 2 directory.
+		static where is stored the css code and templates where is stored the html code.
+	5. All the others files .py have the code for this app.
+### Packages 
+
+1. All the packages you need will be on the requirements.txt.
+2. You will need to go on a terminal like visual studio, go on the path of the project where is stored the requirement.txt and pip install requirement.txt.
+
+### Data
+
+1. Data scrapped 
+	1. All the data are scrapped from league of leagends : https://www.leagueofgraphs.com/
+
+## Conclusion
+
+1. you will have a simple dashboard with a little introduction of what is league of legends and some informations of this one.
+2. you will have the 10 best players.
+3. you will have the counters and you can search like what the counters for the champion "Ekko".
+4. you will have the stats for the side and the role like who is the better red or blue side in solo/duo mode.
+
+
+
+
+
+
